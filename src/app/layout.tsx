206b7f5dc019eb6795a933db731d3e9406147bca
unknown <gaikwadomkar460@gmail.com>
2026-02-14 20:55:34 +0530

    Fixed AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,13 +27,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
+    <AuthProvider>
+        <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
           {children}
         </Providers>
       </body>
+    </AuthProvider>
     </html>
   );
 }
